3e176c91dbe09f9666acd2d6adbca7a1443e9253
Shamil Rasheed <shamilrasheed131@gmail.com>
2022-12-29 11:46:45 +0530

    Moderation service added for filtering comments

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -7,8 +7,21 @@ const CommentList = ({ comments }) => {
             <ul>
                 {
                     comments.map(comment => {
+                        let content;
+                        if(comment.status === 'approved'){
+                            content = comment.content;
+                        }
+
+                        if(comment.status === 'pending'){
+                            content = 'This comment is awaiting moderation!';
+                        }
+
+                        if(comment.status === 'rejected'){
+                            content = 'This comment is rejected!';
+                        }
+
                         return (
-                            <li key={comment?.id}>{comment?.content}</li>
+                            <li key={comment?.id}>{content}</li>
                         )
                     })
                 }
--- event-bus/index.js
+++ event-bus/index.js
@@ -14,9 +14,10 @@ app.post('/events', (req, res) => {
         axios.post('http://localhost:4000/events', event)
         axios.post('http://localhost:4001/events', event);
         axios.post('http://localhost:4002/events', event);
+        axios.post('http://localhost:4003/events', event);
     } catch (error) {
         console.log(error);
-        res.json(error)
+        res.json({ error });
     }
 
     res.send({ status: 'OK' });
