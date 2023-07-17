513a2aa5c33ad0d54dd3d6c1ef9b51a04e71fd12
Shamil Rasheed <shamilrasheed131@gmail.com>
2023-07-17 11:50:09 +0530

    Added yaml file for posts to create pods

--- comments/index.js
+++ comments/index.js
@@ -14,26 +14,31 @@ app.get('/posts/:id/comments', (req, res) => {
 });
 
 app.post('/posts/:id/comments', async (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
+    try {
+        const commentId = randomBytes(4).toString('hex');
+        const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || [];
+        const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content, status: 'pending' });
-    commentsByPostId[req.params.id] = comments;
+        comments.push({ id: commentId, content, status: 'pending' });
+        commentsByPostId[req.params.id] = comments;
 
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id,
-            status: 'pending',
-        }
-    })
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentCreated',
+            data: {
+                id: commentId,
+                content,
+                postId: req.params.id,
+                status: 'pending',
+            }
+        })
 
-    res.status(201).json(comments);
+        res.status(201).json(comments);
+    } catch (error) {
+        console.log('Error in comments index post method');
+        res.send(commentsByPostId);
+    }
 });
 
 
